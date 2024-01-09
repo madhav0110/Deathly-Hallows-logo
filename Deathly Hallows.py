12a203b7151f8d5193570a26d164f07c3431e8e8
MADHAV <wadhwanamadhav@gmail.com>
2024-01-10 00:03:12 +0530

    Add files via upload

---
+++ Deathly Hallows.py
@@ -0,0 +1,25 @@
+import turtle
+
+bob = turtle.Turtle()
+
+bob.speed(10)
+
+for i in range(3):
+
+  bob.forward(200)
+
+  bob.left(120)
+
+bob.forward(95)
+
+for i in range(36):
+
+  bob.forward(10)
+
+  bob.left(10)
+
+bob.forward(6)
+
+bob.left(90)
+
+bob.forward(172)
